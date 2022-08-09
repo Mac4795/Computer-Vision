999dd4fe5b4439dc86c37094ee3614ba66167677
Mac4795 <m.chizea@hotmail.com>
2022-08-09 21:06:38 +0100

    Adding .h5 and .txt files.

--- manual_rps.py
+++ manual_rps.py
@@ -1,1 +1,12 @@
+import random 
 
+
+class RockPaperScissors():
+    def __init__(self):
+        self.guess = input()
+
+    def get_computer_choice(self):
+        self.computer_guess = input()
+    
+    def get_user_choice(self):
+        self.user_choice = input()
