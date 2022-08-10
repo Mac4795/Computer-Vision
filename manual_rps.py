164a49ba134b210badff00a2913751cf4ba0951d
Mac4795 <m.chizea@hotmail.com>
2022-08-10 20:40:41 +0100

    Updated game play file

--- manual_rps.py
+++ manual_rps.py
@@ -6,7 +6,21 @@ def get_computer_choice():
     return random.choice(computer_guess)
     
 def get_user_choice():
-    user_guess = input('Please enter a guess; either Rock, Paper, Scissors: ')
-    return user_guess
+    user_guess = input('Please enter a guess; either Rock, Paper or Scissors: ')
+    return user_guess.capitalize()
 
-#def 
+def get_winner(computer, player):
+    print(f'Computer chose {computer}')
+    if player == 'Rock' and computer == 'Scissors' or player == 'Paper' and computer == 'Rock' or player == 'Scisors' and computer == 'Paper':
+        return 'Congratulations! You beat the computer!'
+    elif player == computer:
+        return 'Looks like its a draw.'
+    else:
+        return 'The computer has won. Better luck next time.'
+
+def play():
+    player = get_user_choice()
+    computer = get_computer_choice()
+    print(get_winner(computer, player))
+
+play()
